09e979572aaeb79b21acd446418631208097dac0
Mahesh Umachagi <Mahesh-Umachagi@github.com>
2020-04-29 20:15:10 +0530

    final commit

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CompaniesController } from './companies.controller';
 import { CompaniesService } from './companies.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Company } from './company.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Company])],
   controllers: [CompaniesController],
-  providers: [CompaniesService]
+  providers: [CompaniesService],
 })
 export class CompaniesModule {}
--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Company } from './company.entity';
 
 @Injectable()
-export class CompaniesService {}
+export class CompaniesService extends TypeOrmCrudService<Company> {
+  constructor(@InjectRepository(Company) repo) {
+    super(repo);
+  }
+}
